soitun/strongMan

commit 7b61664a1b7679e146c20c4b0d27fc145f8cd664

Create and commit initial migrations
These should really be in the repository to allow existing installations
to get updated easily.

setup.py
@@ -80,16 +80,6 @@ class Migrator(BaseInstaller):
 
     def migrate(self):
         self._run_bash(
-            self.env + "/bin/python " + self.django_dir + "/manage.py makemigrations certificates --settings=strongMan.settings.local")
-        self._run_bash(
-            self.env + "/bin/python " + self.django_dir + "/manage.py makemigrations connections --settings=strongMan.settings.local")
-        self._run_bash(
-            self.env + "/bin/python " + self.django_dir + "/manage.py makemigrations eap_secrets --settings=strongMan.settings.local")
-        self._run_bash(
-            self.env + "/bin/python " + self.django_dir + "/manage.py makemigrations pools --settings=strongMan.settings.local")
-        self._run_bash(
-            self.env + "/bin/python " + self.django_dir + "/manage.py makemigrations server_connections --settings=strongMan.settings.local")
-        self._run_bash(
             self.env + "/bin/python " + self.django_dir + "/manage.py migrate --settings=strongMan.settings.local")
 
     def load_fixtures(self):

.gitignore
@@ -1,6 +1,5 @@
 # Byte-compiled / optimized / DLL files
 __pycache__/
-migrations/
 *.py[cod]
 *$py.class
 *.pyc

strongMan/apps/certificates/migrations/0001_initial.py
@@ -1,0 +1,125 @@
+# Generated by Django 4.2.26
+
+from django.db import migrations, models
+import django.db.models.deletion
+import strongMan.apps.certificates.models.core
+import strongMan.helper_apps.encryption.fields
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AbstractIdentity',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            ],
+            bases=(strongMan.apps.certificates.models.core.DjangoAbstractBase, strongMan.apps.certificates.models.core.CertificateModel, models.Model),
+        ),
+        migrations.CreateModel(
+            name='Certificate',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('der_container', strongMan.helper_apps.encryption.fields.EncryptedField()),
+                ('type', models.CharField(max_length=10)),
+                ('algorithm', models.CharField(max_length=3)),
+                ('public_key_hash', models.TextField()),
+                ('serial_number', models.TextField()),
+                ('hash_algorithm', models.CharField(max_length=20)),
+                ('is_CA', models.BooleanField()),
+                ('valid_not_after', models.DateTimeField()),
+                ('valid_not_before', models.DateTimeField()),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(strongMan.apps.certificates.models.core.CertificateModel, models.Model, strongMan.apps.certificates.models.core.DjangoAbstractBase),
+        ),
+        migrations.CreateModel(
+            name='DistinguishedName',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('blob', models.BinaryField()),
+                ('location', models.TextField()),
+                ('country', models.TextField()),
+                ('email', models.TextField()),
+                ('organization', models.TextField()),
+                ('unit', models.TextField()),
+                ('cname', models.TextField()),
+                ('province', models.TextField()),
+            ],
+            bases=(strongMan.apps.certificates.models.core.CertificateModel, models.Model),
+        ),
+        migrations.CreateModel(
+            name='PrivateKey',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('der_container', strongMan.helper_apps.encryption.fields.EncryptedField()),
+                ('type', models.CharField(max_length=10)),
+                ('algorithm', models.CharField(max_length=3)),
+                ('public_key_hash', models.TextField()),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(strongMan.apps.certificates.models.core.CertificateModel, models.Model),
+        ),
+        migrations.CreateModel(
+            name='DnIdentity',
+            fields=[
+                ('abstractidentity_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='certificates.abstractidentity')),
+            ],
+            bases=('certificates.abstractidentity',),
+        ),
+        migrations.CreateModel(
+            name='TextIdentity',
+            fields=[
+                ('abstractidentity_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='certificates.abstractidentity')),
+                ('text', models.TextField()),
+            ],
+            bases=('certificates.abstractidentity',),
+        ),
+        migrations.CreateModel(
+            name='ViciCertificate',
+            fields=[
+                ('certificate_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='certificates.certificate')),
+                ('has_private_key', models.BooleanField(default=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('certificates.certificate',),
+        ),
+        migrations.AddField(
+            model_name='certificate',
+            name='issuer',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='certificate_issuer', to='certificates.distinguishedname'),
+        ),
+        migrations.AddField(
+            model_name='certificate',
+            name='subject',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='certificate_subject', to='certificates.distinguishedname'),
+        ),
+        migrations.AddField(
+            model_name='abstractidentity',
+            name='certificate',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='ident_%(app_label)s_%(class)s', to='certificates.certificate'),
+        ),
+        migrations.CreateModel(
+            name='UserCertificate',
+            fields=[
+                ('certificate_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='certificates.certificate')),
+                ('_nickname', models.TextField()),
+                ('private_key', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='certificates', to='certificates.privatekey')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('certificates.certificate',),
+        ),
+    ]

strongMan/apps/connections/migrations/0001_initial.py
@@ -1,0 +1,167 @@
+# Generated by Django 4.2.26
+
+from django.db import migrations, models
+import django.db.models.deletion
+import strongMan.helper_apps.encryption.fields
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('certificates', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Authentication',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.TextField()),
+                ('auth', models.TextField()),
+                ('round', models.IntegerField(default=1)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Child',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.TextField()),
+                ('mode', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Connection',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('profile', models.TextField(unique=True)),
+                ('auth', models.TextField()),
+                ('version', models.IntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='AutoCaAuthentication',
+            fields=[
+                ('authentication_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.authentication')),
+                ('ca_identity', models.TextField()),
+            ],
+            bases=('connections.authentication',),
+        ),
+        migrations.CreateModel(
+            name='EapAuthentication',
+            fields=[
+                ('authentication_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.authentication')),
+                ('eap_id', models.TextField()),
+            ],
+            bases=('connections.authentication',),
+        ),
+        migrations.CreateModel(
+            name='IKEv2Certificate',
+            fields=[
+                ('connection_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.connection')),
+            ],
+            bases=('connections.connection',),
+        ),
+        migrations.CreateModel(
+            name='IKEv2CertificateEAP',
+            fields=[
+                ('connection_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.connection')),
+            ],
+            bases=('connections.connection',),
+        ),
+        migrations.CreateModel(
+            name='IKEv2EAP',
+            fields=[
+                ('connection_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.connection')),
+            ],
+            bases=('connections.connection',),
+        ),
+        migrations.CreateModel(
+            name='IKEv2EapTls',
+            fields=[
+                ('connection_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.connection')),
+            ],
+            bases=('connections.connection',),
+        ),
+        migrations.CreateModel(
+            name='Secret',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('type', models.TextField()),
+                ('data', strongMan.helper_apps.encryption.fields.EncryptedCharField(max_length=50)),
+                ('authentication', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='authentication', to='connections.authentication')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Proposal',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('type', models.TextField()),
+                ('child', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='esp_proposals', to='connections.child')),
+                ('connection', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='proposals', to='connections.connection')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='LogMessage',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('message', models.TextField()),
+                ('connection', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to='connections.connection')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='child',
+            name='connection',
+            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='children', to='connections.connection'),
+        ),
+        migrations.AddField(
+            model_name='authentication',
+            name='local',
+            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='local', to='connections.connection'),
+        ),
+        migrations.AddField(
+            model_name='authentication',
+            name='remote',
+            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='remote', to='connections.connection'),
+        ),
+        migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('value', models.TextField()),
+                ('local_addresses', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='local_addresses', to='connections.connection')),
+                ('local_ts', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='local_ts', to='connections.child')),
+                ('remote_addresses', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='remote_addresses', to='connections.connection')),
+                ('remote_ts', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='remote_ts', to='connections.child')),
+                ('vips', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='vips', to='connections.connection')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='EapTlsAuthentication',
+            fields=[
+                ('authentication_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.authentication')),
+                ('eap_id', models.TextField()),
+                ('identity', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='tls_identity', to='certificates.abstractidentity')),
+            ],
+            bases=('connections.authentication',),
+        ),
+        migrations.CreateModel(
+            name='CertificateAuthentication',
+            fields=[
+                ('authentication_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.authentication')),
+                ('identity', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='cert_identity', to='certificates.abstractidentity')),
+            ],
+            bases=('connections.authentication',),
+        ),
+        migrations.CreateModel(
+            name='CaCertificateAuthentication',
+            fields=[
+                ('authentication_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='connections.authentication')),
+                ('ca_identity', models.TextField()),
+                ('ca_cert', models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='ca_cert_authentication', to='certificates.usercertificate')),
+            ],
+            bases=('connections.authentication',),
+        ),
+    ]

strongMan/apps/eap_secrets/migrations/0001_initial.py
@@ -1,0 +1,25 @@
+# Generated by Django 4.2.26
+
+from django.db import migrations, models
+import strongMan.helper_apps.encryption.fields
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Secret',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.TextField(unique=True)),
+                ('type', models.TextField()),
+                ('password', strongMan.helper_apps.encryption.fields.EncryptedCharField(max_length=50)),
+                ('salt', models.TextField()),
+            ],
+        ),
+    ]

strongMan/apps/pools/migrations/0001_initial.py
@@ -1,0 +1,24 @@
+# Generated by Django 4.2.26
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Pool',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('poolname', models.TextField(unique=True)),
+                ('addresses', models.TextField()),
+                ('attribute', models.CharField(choices=[(None, 'None'), ('dns', 'dns'), ('nbns', 'nbns'), ('dhcp', 'dhcp'), ('netmask', 'netmask'), ('server', 'server'), ('subnet', 'subnet'), ('split_include', 'split_include'), ('split_exclude', 'split_exclude')], max_length=56, null=True)),
+                ('attributevalues', models.TextField(null=True)),
+            ],
+        ),
+    ]

commit 58eb14372e137fa2c53b8d7dd3df5a508d833990

Don't delete migrations with install command
No idea why this step was there, but I guess there were no actual
migrations before the referenced commit.

Closes #169.

Fixes: 7b61664a1b76 ("Create and commit initial migrations")

setup.py
@@ -65,15 +65,6 @@ class BaseInstaller(object):
 
 
 class Migrator(BaseInstaller):
-    def delete_migrations(self):
-        for entries in os.walk(self.django_dir + "/strongMan"):
-            dir = list(entries)[0]
-            if str(dir).__contains__('migrations'):
-                if os.path.exists(dir):
-                    if not self.mute:
-                        print(self._tab("Delete " + dir))
-                    shutil.rmtree(dir)
-
     def delete_db(self):
         print(self._tab("Delete " + "strongMan/db.sqlite3"))
         os.remove(self.django_dir + "/strongMan/db.sqlite3")
@@ -97,10 +88,6 @@ class Installer(BaseInstaller):
     def migrate_db(self):
         migrator = Migrator()
         migrator.mute = self.mute
-        try:
-            migrator.delete_migrations()
-        except Exception as e:
-            print(e)
         try:
             migrator.delete_db()
         except Exception as e: